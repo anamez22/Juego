ff013892c0ac63844f75344bd3363ec38b72bc42
Vae29 <valesalvalen@gmail.com>
2025-06-10 03:07:34 -0500

    avance tuberias y cambios de interfaz

--- views/juego.py
+++ views/juego.py
@@ -3,7 +3,19 @@ from tkinter import *
 from views.Tooltip import Tooltip
 from PIL import Image, ImageTk
 
+from Controllers.Tuberias import Tuberias
+
 class Juego():
+
+    def comenzarJuego(self,event):
+        self.tuberia = Tuberias(self.lienzo)
+        #aca puede crear el objeto de la clase pajaro q va a crear para que ejecute las funciones suyas
+        #porque pues esta funcion es la que se ejecuta al presionar el boton jugar
+
+        #aqui vamos a ejcutar las funciones de las clases que se creen en otros archivos 
+        
+
+
     def paint(self):
         cuerpo = self.lienzo.create_oval(10, 270, 75, 330, fill="#fcf83b", tags="cuerpo")
         ala = self.lienzo.create_oval(7, 295, 37, 315, fill="#ffffff", tags="ala")
@@ -18,36 +30,34 @@ class Juego():
     def __init__(self):
         self.ventana = tk.Tk()
         self.ventana.title("JUEGO HAPPY BIRD")
-        self.ventana.config(width=900, height=700, bg="#000102")
+        self.ventana.config(width=900, height=700, bg="#000000")
         self.ventana.resizable(0,0)
 
         self.lienzo = tk.Canvas(self.ventana, bg="#94caca")
-        self.lienzo.place(relx=0.5, rely=0.5, anchor="center", width=800, height=600)
+        self.lienzo.place(relx=0.5, rely=0.5, anchor="center", width=800, height=640)
 
-        imagenTronco = Image.open(r"Juego\icons\woods.png")
-        imagentk = ImageTk.PhotoImage(imagenTronco)
-        image = self.lienzo.create_image(300, 445, image=imagentk) 
-
-        self.iconoJugar = tk.PhotoImage(file=r"Juego\icons\icons8-play-40.png")
+        self.iconoJugar = tk.PhotoImage(file=r"Juego\icons\icono_play.png")
         self.iconoAyuda = tk.PhotoImage(file=r"Juego\icons\icons8-help-50.png")
         self.iconoNube = tk.PhotoImage(file=r"Juego\icons\icons8-clouds-100.png")
         
-        self.btnJugar = tk.Button(self.ventana, image=self.iconoJugar)
-        self.btnJugar.place(relx=0.5, rely=1, y=-27, anchor="center", width=40, height=40)
+        self.btnJugar = tk.Button(self.ventana, image=self.iconoJugar, bg="#dde38d")
+        self.btnJugar.place(relx=0.5, rely=1, y=-53, anchor="center", width=105, height=44)
         Tooltip(self.btnJugar, "Presione para iniciar el juego")
 
         self.btnAyuda = tk.Button(self.ventana, image=self.iconoAyuda)
-        self.btnAyuda.place(width=50, height=50, x=780, y=70)
+        self.btnAyuda.place(width=50, height=50, x=780, y=50)
         Tooltip(self.btnAyuda, "Presione para ver la ayuda")
 
-        self.lblIconoNube = tk.Label(self.ventana, image=self.iconoNube, bg="#94caca")
+        self.lblIconoNube = tk.Label(self.lienzo, image=self.iconoNube, bg="#94caca")
         self.lblIconoNube.place(width=100, height=100, relx=0.5, y=90, anchor="n")
         
         self.lblPuntaje = tk.Label(self.lienzo, text="Puntaje", font= ("Arial", 14))
         self.lblPuntaje.place(relx=0.5, y=30, anchor="center")
 
-        self.lienzo.create_rectangle(0, 570, 800, 600, fill="#f6ff7a", outline="#4fe729", width=5)
-
+        self.lienzo.create_rectangle(0, 590, 800, 660, fill="#dde38d", outline="#33b812", width=5)
         
         self.paint()
+        self.btnJugar.bind("<Button-1>", self.comenzarJuego)
+
+
         self.ventana.mainloop()
