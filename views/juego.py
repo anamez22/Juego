08705178e6789cb7fe105bf0fc7349f066072c1b
Vae29 <valesalvalen@gmail.com>
2025-06-10 21:50:31 -0500

    Tuberias funcionales

--- views/juego.py
+++ views/juego.py
@@ -9,6 +9,7 @@ class Juego():
 
     def comenzarJuego(self,event):
         self.tuberia = Tuberias(self.lienzo)
+        self.tuberia.iniciar_movimiento()
         #aca puede crear el objeto de la clase pajaro q va a crear para que ejecute las funciones suyas
         #porque pues esta funcion es la que se ejecuta al presionar el boton jugar
 
@@ -38,7 +39,6 @@ class Juego():
 
         self.iconoJugar = tk.PhotoImage(file=r"Juego\icons\icono_play.png")
         self.iconoAyuda = tk.PhotoImage(file=r"Juego\icons\icons8-help-50.png")
-        self.iconoNube = tk.PhotoImage(file=r"Juego\icons\icons8-clouds-100.png")
         
         self.btnJugar = tk.Button(self.ventana, image=self.iconoJugar, bg="#dde38d")
         self.btnJugar.place(relx=0.5, rely=1, y=-53, anchor="center", width=105, height=44)
@@ -47,14 +47,11 @@ class Juego():
         self.btnAyuda = tk.Button(self.ventana, image=self.iconoAyuda)
         self.btnAyuda.place(width=50, height=50, x=780, y=50)
         Tooltip(self.btnAyuda, "Presione para ver la ayuda")
-
-        self.lblIconoNube = tk.Label(self.lienzo, image=self.iconoNube, bg="#94caca")
-        self.lblIconoNube.place(width=100, height=100, relx=0.5, y=90, anchor="n")
-        
+ 
         self.lblPuntaje = tk.Label(self.lienzo, text="Puntaje", font= ("Arial", 14))
         self.lblPuntaje.place(relx=0.5, y=30, anchor="center")
 
-        self.lienzo.create_rectangle(0, 590, 800, 660, fill="#dde38d", outline="#33b812", width=5)
+        self.lienzo.create_rectangle(0, 590, 800, 640, fill="#dde38d", outline="#33b812", width=5)
         
         self.paint()
         self.btnJugar.bind("<Button-1>", self.comenzarJuego)
